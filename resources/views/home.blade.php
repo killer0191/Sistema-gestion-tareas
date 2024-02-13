526f02710f0abb737bc609666c072dccab44a930
demon0191 <adrymoises.arias.morales@gmail.com>
2024-02-13 10:37:54 -0600

    finalización de la edición de los estilos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -61,20 +61,25 @@
           <div class="card mb-3" id="tarea-{{ $tarea->idTarea }}">
             <img src="{{ asset('assets/' . $imagen) }}" class="estado-imagen" alt="Estado" width="50px">
             <div class="card-body">
-              <h5 class="card-title">{{ $tarea->titulo }}</h5>
+              <div class="d-flex justify-content-between">
+                <h5 class="card-title">{{ $tarea->titulo }}</h5>
+                <div class="btn-group ml-auto">
+                  <form method="POST" class="eliminar-tarea-form" action="/tarea/{{ $tarea->idTarea }}">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-accion eliminar-tarea-btn" data-action="eliminar"
+                      data-id="{{ $tarea->idTarea }}">Eliminar</button>
+                  </form>
+                  <button class="btn btn-accion editar-tarea-btn" data-toggle="modal" data-target="#editarTareaModal"
+                    data-tarea='{{ json_encode($tarea) }}' data-id="{{ $tarea->idTarea }}">Editar</button>
+                </div>
+
+
+              </div>
               <p class="card-text">{{ $tarea->descripcion }}</p>
               @if($tarea->fechaVenc)
-              <p class="card-text">Fecha de vencimiento: {{ $tarea->fechaVenc }}</p>
+              <p class="card-text fecha-vencimiento">Fecha de vencimiento: {{ $tarea->fechaVenc }}</p>
               @endif
-              <form method="POST" class="eliminar-tarea-form" action="/tarea/{{ $tarea->idTarea }}">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger eliminar-tarea-btn" data-action="eliminar"
-                  data-id="{{ $tarea->idTarea }}">Eliminar</button>
-              </form>
-              <button class="btn btn-primary mt-2 editar-tarea-btn" data-toggle="modal" data-target="#editarTareaModal"
-                data-tarea="{{ json_encode($tarea) }}" data-id="{{ $tarea->idTarea }}">Editar</button>
-              <div id="editarTareaContainer-{{ $tarea->idTarea }}"></div>
             </div>
           </div>
           @endforeach
@@ -192,45 +197,38 @@
         data: datos,
         success: function(response) {
           console.log("Tarea creada exitosamente");
+
           // Cerrar el modal de crear tarea
           $('#crearTareaModal').modal('hide');
+
           // Limpiar los campos del formulario
           $("#crearTareaForm")[0].reset();
 
-          var imagen = '';
-          if (response.idEstadoF == 1) {
-            imagen = 'finalizada.png';
-          } else {
-            var fechaActual = new Date();
-            var fechaVencimiento = new Date(response.fechaVenc);
-            if (fechaActual > fechaVencimiento) {
-              imagen = 'vencida.png';
-            } else {
-              imagen = 'pendiente.png';
-            }
-          }
-
-          // Agregar la nueva tarea a la lista
+          // Generar el HTML para la nueva tarea
+          var imagen = response.idEstadoF == 1 ? 'finalizada.png' : response.fechaVenc ? 'pendiente.png' :
+            'vencida.png';
           var nuevaTarea = `
         <div class="card mb-3">
-        <img src="{{ asset('assets/') }}/${imagen}" class="estado-imagen" alt="Estado" width="50px">
-          <div class="card-body">
-            <h5 class="card-title">${response.titulo}</h5>
-            <p class="card-text">${response.descripcion}</p>
-            ${response.fechaVenc ? `<p class="card-text">Fecha de vencimiento: ${response.fechaVenc}</p>` : ''}
-            <form method="POST" class="eliminar-tarea-form" action="/tarea/${response.idTarea}">
-              @csrf
-              @method('DELETE')
-              <button type="submit" class="btn btn-danger" data-id="${response.idTarea}">Eliminar</button>
-            </form>
-            <button class="btn btn-primary mt-2 editar-tarea-btn" data-toggle="modal" data-target="#editarTareaModal"
-              data-tarea='${JSON.stringify(response)}' data-id="${response.idTarea}">Editar</button>
-          </div>
+            <img src="{{ asset('assets/') }}/${imagen}" class="estado-imagen" alt="Estado" width="50px">
+            <div class="card-body">
+                <h5 class="card-title">${response.titulo}</h5>
+                <p class="card-text">${response.descripcion}</p>
+                ${response.fechaVenc ? `<p class="card-text">Fecha de vencimiento: ${response.fechaVenc}</p>` : ''}
+                <form method="POST" class="eliminar-tarea-form" action="/tarea/${response.idTarea}">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger" data-id="${response.idTarea}">Eliminar</button>
+                </form>
+                <button class="btn btn-primary mt-2 editar-tarea-btn" data-toggle="modal" data-target="#editarTareaModal"
+                    data-tarea='${JSON.stringify(response)}' data-id="${response.idTarea}">Editar</button>
+            </div>
         </div>
-      `;
-          $(".row").append(nuevaTarea);
+    `;
 
+          // Agregar la nueva tarea al contenedor de tareas existente
+          $(".card-container").append(nuevaTarea);
         },
+
         error: function(xhr, status, error) {
           console.error("Error al crear la tarea");
         },
